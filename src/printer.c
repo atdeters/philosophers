63a73b10d7a6196c31e7903c39f3a02dfd1dc09a
atdeters <sreted@outlook.com>
2025-04-25 16:35:39 +0200

    style: Delete empty line from p_log function
    Fixed for norminette

--- src/printer.c
+++ src/printer.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:14:34 by adeters           #+#    #+#             */
-/*   Updated: 2025/04/25 16:31:05 by adeters          ###   ########.fr       */
+/*   Updated: 2025/04/25 16:35:27 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	p_str_fd(int fd, char *str)
 
 bool	p_log(t_data *data, int philo_nb, int action, pthread_mutex_t *mutexes)
 {
-	
 	pthread_mutex_lock(&mutexes[data->nbp + 1]);
 	if (data->is_kil)
 		return (pthread_mutex_unlock(&mutexes[data->nbp + 1]), false);
