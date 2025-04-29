19bacdafece19a01c2fc2c2cd13dba15302cffe5
atdeters <sreted@outlook.com>
2025-04-29 19:09:51 +0200

    fix: Manipulate thinking time for tte > tts
    If the time to eat is bigger than the time to sleep the current
    fix does not work as the thinking time will not be long enough
    to make one philosopher win the fight over another. By adding tte-tts
    this can be fixed.

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 01:34:34 by andreas           #+#    #+#             */
-/*   Updated: 2025/04/29 14:19:09 by adeters          ###   ########.fr       */
+/*   Updated: 2025/04/29 19:05:48 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 void	act_manipulate_think(t_philo *p)
 {
+	unsigned int	extra;
+
 	if (p->philo_nb % 2 == 0 && p->times_eaten == 0 && usleep(30000))
 		do_on_sleep_fail(&p);
 	if (p->data->nbp % 2 != 0 && p->philo_nb == get_next_delay(p->data))
 	{
 		if (p->data->nbp == 1 || p->data->nbp == 2)
 			return ;
-		if (usleep(20000))
+		extra = 0;
+		if	(p->data->tte > p->data->tts)
+			extra = p->data->tte - p->data->tts;
+		if (usleep(20000 + extra))
 		{
 			do_on_sleep_fail(&p);
 			return ;
